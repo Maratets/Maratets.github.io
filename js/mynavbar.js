c1400b5cb314215765e78493cdc7592ae35cbe72
Maratets <maratabv@yandex.ru>
2017-05-22 16:46:11 +0300

    delete

--- js/mynavbar.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  $(window).scroll(function () {
-      var top = $(document).scrollTop();
-      $('.splash').css({
-        'background-position': '0px -'+(top/3).toFixed(2)+'px'
-      });
-  });
-})();
